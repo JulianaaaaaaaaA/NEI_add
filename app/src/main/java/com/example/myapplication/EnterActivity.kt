c6fdd13111b7a8ba57e8872b371b246644c1c7fb
JulianaaaaaaaaA <ulya.yakovleva.0404@inbox.ru>
2022-07-25 15:06:21 +0300

    test commit

--- app/src/main/java/com/example/myapplication/EnterActivity.kt
+++ app/src/main/java/com/example/myapplication/EnterActivity.kt
@@ -7,5 +7,6 @@ class EnterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter)
+
     }
 }
